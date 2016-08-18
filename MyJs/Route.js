9aed50691edff29a6b8d0849481773252e678cde
adeng <you@example.com>
2016-08-18 11:16:45 +0800

    'MyRedux1'

--- MyJs/Route.js
+++ MyJs/Route.js
@@ -5,7 +5,6 @@ import { Router, Route, hashHistory } from 'react-router'
 import PropsAndState from './PropsAndState/PropsAndState.js';
 import Test from './Animation/Animation.js';
 import Carousel from './Animation/Carousel.js';
-import Redux from './Redux/Redux.js';
 import ReduxForm from './ReduxForm/ReduxForm.js';
 import MyRedux from './MyRedux/index.js';
 
@@ -17,7 +16,6 @@ render((
         <Route path="/props" /*组件内注释*/  component={PropsAndState} />
         <Route path="/ani/1" /*组件内注释*/  component={Test} />
         <Route path="/ani/2" /*组件内注释*/  component={Carousel}/>
-        <Route path="/redux" /*组件内注释*/  component={Redux}/>
         <Route path="/redux-form/:type"  component={ReduxForm}/>
         <Route path="/my-redux"  component={MyRedux}/>
     </Router>
